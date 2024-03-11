f13281b6f621ed79a90d05d31c09bcded2372c1a
zamorini <pedrozamorini@gmail.com>
2024-03-10 21:16:25 -0300

    script refatorado com chamadas publicas cnpqs e pdfs dos editais

--- 03 - beautifulsoup II/cnpq_ii.py
+++ 03 - beautifulsoup II/cnpq_ii.py
@@ -6,31 +6,20 @@ response = requests.get('http://memoria2.cnpq.br/web/guest/chamadas-publicas?p_p
 content = response.content
 
 site = BeautifulSoup(content, 'html.parser')
-# primeiro parâmetro é o conteudo, segundo é dizendo ao beautifulsoup que o conteúdo
-# é em html
 
-#print(site.prettify())
-
- # CHAMADAS PÚBLICAS CNPQ 
-
+# CHAMADAS PÚBLICAS CNPQ 
 chamadas = site.findAll('div', attrs={'class': 'content'}) 
 pdfs_chamadas = site.findAll('div', attrs={'class': 'links-normas pull-left'})
-# tag, parâmetro {dict}
 
-for chamada in chamadas: 
-  # print(chamada.text)
+for chamada, pdf_chamada in zip(chamadas, pdfs_chamadas):
+    titulo = chamada.find('h4')
+    descricao = chamada.find('p')
+    data = chamada.find('div', attrs={'class': 'inscricao'})
+    link_edital = pdf_chamada.find('a', attrs={'class': 'btn'})
 
-  # TITULO DA CHAMADA 
-  titulo = chamada.find('h4')
+    if titulo and descricao and data and link_edital:
+        print(f"Título: {titulo.text}\nDescrição: {descricao.text}\nData: {data.text}\nLink do PDF: {link_edital['href']}\n")
 
-  # DESCRIÇÃO 
-  descricao = chamada.find('p')
-
-  #DATA 
-  data = chamada.find('div', attrs={'class': 'inscricao'})
-
-for pdf_chamada in pdfs_chamadas:
-  link_edital = pdf_chamada.find('a', attrs={'class': 'btn'})
-
-print(titulo.text + '\n' + descricao.text + '\n' + data.text
-      + '\n' + f"Chamada: {link_edital['href']}")
+# a função zip serve para iterar simultaneamente sobre as listas de chamada
+# e links de PDFs. Isso permite que você acesse os elementos correspondentes de 
+# ambas as listas na mesma iteração.
