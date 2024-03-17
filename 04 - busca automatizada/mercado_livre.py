8c1a880237c6cf570e2acfd2dcb7a6b4b7d861ef
zamorini <pedrozamorini@gmail.com>
2024-03-17 02:28:47 -0300

    busca automatizada de produtos

--- 04 - busca automatizada/mercado_livre.py
+++ 04 - busca automatizada/mercado_livre.py
@@ -12,6 +12,23 @@ response = requests.get(url_base + nome_produto)
 
 site = BeautifulSoup(response.text, 'html.parser')
 
-produto = site.find('div', attrs={'class': 'andes-card ui-search-result ui-search-result--core andes-card--flat andes-card--padding-16'})
+produtos = site.findAll('div', attrs={'class': 'andes-card ui-search-result ui-search-result--core andes-card--flat andes-card--padding-16'})
 
-print(produto.prettify())
+for produto in produtos: 
+  titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
+  link = produto.find('a', attrs={'class': 'ui-search-item__group__element ui-search-link__title-card ui-search-link'})
+
+  money_symbol = produto.find('span', attrs={'class': 'andes-money-amount__currency-symbol'})
+  money_fraction = produto.find('span', attrs={'class': 'andes-money-amount__fraction'})
+  money_cents = produto.find('span', attrs={'class': 'andes-money-amount__cents andes-money-amount__cents--superscript-24'})
+
+  # print(produto.prettify())
+  print('Titulo do produto:', titulo.text)
+  print('Link do produto:', link['href'])
+
+  if money_symbol and money_fraction and money_cents:
+    print('Preço do produto:', money_symbol.text + '' + money_fraction.text + ',' + money_cents.text)
+  else:
+    print('Preço do produto:', money_symbol.text + '' + money_fraction.text)
+
+  print('\n\n')
